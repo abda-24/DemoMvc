9125647948d446b7722e30279d3ab748b2ef6200
Elbanna <contact.elbana@gmail.com>
2025-08-01 02:47:27 +0300

    Add project files.

---
+++ Demo.DAL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo.DAL.Repositories;
+public class EmployeeRepository
+    : GenericRepository<Employee>
+    , IEmployeeRepository
+{
+    public EmployeeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public IEnumerable<Employee> GetAll(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
